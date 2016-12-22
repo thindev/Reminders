67e8713a84ce4ae7c9f3e66a986e81af1bfb3331
thindev <838876225@qq.com>
2016-12-22 18:59:59 +0800

    Add "new reminder" adn exit menu options

--- app/src/main/java/com/apress/gerber/reminders/RemindersActivity.java
+++ app/src/main/java/com/apress/gerber/reminders/RemindersActivity.java
@@ -2,6 +2,10 @@ package com.apress.gerber.reminders;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -17,5 +21,31 @@ ListView mListView;
         ArrayAdapter<String> arrayAdapter=new ArrayAdapter<>(this,R.layout.reminders_row,R.id.row_text,
                 new String[]{"fist record","second record","third record"});
         mListView.setAdapter(arrayAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=new MenuInflater(this);
+        inflater.inflate(R.menu.activity_reminders,menu);
+        return  super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.action_new:
+            {
+                Log.d(getLocalClassName(),"create new reminder.");
+                return  true;
+            }
+            case R.id.action_exit:
+            {
+                finish();
+                return true;
+            }
+            default:
+                return  false;
+        }
     }
 }
